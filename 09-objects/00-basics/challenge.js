b85d03906498897fe068ba9df3424b3bd3becb04
Brad Ballard <brad@bradballard.com>
2014-08-23 13:32:42 +0000

    Work on 09-objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,44 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  var myVar = {
+    newProp: newValue
+  };
+  var myVar = object;
+  return myVar;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter)  {
+  var newLetter = {
+    recipient: letter,
+    sender: letter,
+    msg: letter
+  };
+  return newLetter = "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+// module.exports.canIGet = function(item, money) {
+//   return {
+//     'MacBook Air': (>= money),
+//     'MacBook Pro': money,
+//     'Mac Pro': money,
+//     'Apple Sticker': money
+//   };
+  
+//   return {
+//     macBookAir: (newItem['MacBook Air'] >= 999),
+//     macBookPro: (newItem['MacBook Pro'] >= 1299),
+//     macPro: (newItem['Mac Pro'] >= 2499),
+//     appleSticker: (newItem['Apple Sticker'] >= 1),
+//   }
+
+//   return combine;
+
+// };
