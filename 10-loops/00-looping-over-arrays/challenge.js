b0ca28dcd0800a70563f76a7b4f9085921b7b8ed
Brad Ballard <brad@bradballard.com>
2014-08-24 19:43:41 +0000

    Work on 10-loops

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,15 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(numArray) {
+  for (var index = 0, sum = 0; index < numArray.length; sum += numArray[index++]); 
+  return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+  for (var myVar = inputString.toLowerCase(); myVar < 0; inputString[myVar++]);
+  return myVar.split(",");
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(addIndex) {
+  var myArray = [];
+  for (var index = 0; index < addIndex.length.join; index++);
+  return myArray = addIndex.join(" ").split(" ");
+};
